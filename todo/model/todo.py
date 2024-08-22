8893c3c1e2b41b7a1d51697d650262e76e75b987
Kevin Cifuentes <kevins_cifuentes@soy.sena.edu.co>
2024-08-22 12:54:51 -0500

    finalizada la clase TodoBook

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -17,14 +17,41 @@ class Todo:
     def __str__(self) -> str:
         return f"{self.code_id} - {self.title}"
 
+
 class TodoBook:
 
     def __init__(self) -> None:
         self.todos: dict[int, Todo] = {}
 
     def add_todo(self, title: str, description: str) -> int:
+
         id = len(self.todos) + 1
         new_todo = Todo(id, title, description)
         self.todos[id] = new_todo
         
         return id
+    
+    def pending_todos(self) -> list[Todo]:
+
+        pendings: list[Todo] = [pending_todo for pending_todo in self.todos.values() if not pending_todo.completed]
+
+        return pendings
+    
+    def completed_todos(self) -> list[Todo]:
+
+        completed_todos: list[Todo] = [pending_todo for pending_todo in self.todos.values() if pending_todo.completed]
+
+        return completed_todos
+    
+    
+    def tags_todo_count(self) -> dict[str, int]:
+        tags_count: dict[str, int] = {}
+
+        for todo in self.todos.values():
+            for tag in todo.tags:
+                if tag not in tags_count.keys():
+                    tags_count[tag] = 1
+                else:
+                    tags_count[tag] += 1
+
+        return tags_count
