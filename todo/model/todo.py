85180977c29375ae4764f99219f8dbfa17f4b0fe
Kevin Cifuentes <kevins_cifuentes@soy.sena.edu.co>
2024-08-22 12:15:30 -0500

    creacion metodos mark_completed y add_tag

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -5,3 +5,10 @@ class Todo:
         self.description: str = description
         self.completed: bool = False
         self.tags: list[str] = []
+    
+    def mark_completed(self):
+        self.completed = True
+    
+    def add_tag(self, tag: str):
+        if tag not in self.tags: 
+            self.tags.append(tag)
